2fbc76bded746eae8d8245a22d4e7bbf8a61a1cc
TaeHoon <31303655+xoasml@users.noreply.github.com>
2024-05-17 09:07:36 +0900

    Add : 카테고리 자동 생성 및 사이드바 자동 생성

---
+++ _plugins/generate_categories.rb
@@ -0,0 +1,27 @@
+module Jekyll
+    class CategoryPageGenerator < Generator
+      safe true
+  
+      def generate(site)
+        site.categories.each do |category, posts|
+          site.pages << CategoryPage.new(site, site.source, category, posts)
+        end
+      end
+    end
+  
+    class CategoryPage < Page
+      def initialize(site, base, category, posts)
+        @site = site
+        @base = base
+        @dir  = "category/#{category}"
+        @name = 'index.html'
+  
+        self.process(@name)
+        self.read_yaml(File.join(base, '_layouts'), 'category.html')
+        self.data['category'] = category
+        self.data['title'] = category.capitalize
+        self.data['posts'] = posts
+      end
+    end
+  end
+  
